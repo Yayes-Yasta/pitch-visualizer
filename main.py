e77942c8c8ae6ea9dcf276c30026c1ee7ee1d766
Yayes-Yasta <yayesyasta@gmail.com>
2026-03-14 21:39:59 +0400

    The main loop now reads microphone input and puts it into an np array

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pyaudio
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 FRAMES_PER_BUFFER = 1024
@@ -6,16 +8,18 @@ FRAMES_PER_BUFFER = 1024
 def main():
 	pyaudio_object = pyaudio.PyAudio()
 
-	pyaudio_object.open(
+	stream = pyaudio_object.open(
 		format=pyaudio.paFloat32, 
-		channel=1, 
+		channels=1, 
 		rate=44100, 
 		input=True, 
 		frames_per_buffer=FRAMES_PER_BUFFER
 		)
 
 	while True:
-		pass
+		data = stream.read(FRAMES_PER_BUFFER)		
+		# create the array to be used to process the audio
+		frames = np.frombuffer(data, dtype=np.float32) 
 
 if __name__ == '__main__':
 	main()
