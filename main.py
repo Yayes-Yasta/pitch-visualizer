51629ce877be8de0993d632f08f71d4a8fd51f5b
Yayes-Yasta <yayesyasta@gmail.com>
2026-03-15 03:41:14 +0400

    CONSTANTS for pyaudio parameters added

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+FORMAT = pyaudio.paFloat32
+SAMPLE_RATE = 44100 # Hz
 FRAMES_PER_BUFFER = 1024
 
 def main():
 	pyaudio_object = pyaudio.PyAudio()
 
 	stream = pyaudio_object.open(
-		format=pyaudio.paFloat32, 
+		format=FORMAT, 
 		channels=1, 
-		rate=44100, 
+		rate=SAMPLE_RATE, 
 		input=True, 
 		frames_per_buffer=FRAMES_PER_BUFFER
 		)
 
 	while True:
+		# read the sound waves from the microphone
 		data = stream.read(FRAMES_PER_BUFFER)		
 		# create the array to be used to process the audio
 		frames = np.frombuffer(data, dtype=np.float32) 
