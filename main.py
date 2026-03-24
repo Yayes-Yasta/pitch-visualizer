b2d308ca85d0ac5520edc4948e5e54c0049b9051
Yayes-Yasta <yayesyasta@gmail.com>
2026-03-24 18:40:33 +0400

    pygame initialization

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyaudio
 import numpy as np
 from src.pitch_detection import detect_pitch
 import aubio
+import pygame
 
 
 FORMAT = pyaudio.paFloat32
@@ -9,8 +10,13 @@ SAMPLE_RATE = 44100 # Hz
 FRAMES_PER_BUFFER = 2048 # number of frames per stream reading
 
 def main():
-	pyaudio_object = pyaudio.PyAudio()
+	# pygame initialization
+	pygame.init()
+	screen = pygame.display.set_mode((1280, 720))
+	running = True
 
+	# pyaudio initialization
+	pyaudio_object = pyaudio.PyAudio()
 	stream = pyaudio_object.open(
 		format=FORMAT, 
 		channels=1, 
@@ -19,13 +25,19 @@ def main():
 		frames_per_buffer=FRAMES_PER_BUFFER
 		)
 
-	while True:
+	while running:
 		# read the sound waves from the microphone
 		data = stream.read(FRAMES_PER_BUFFER)		
 		# create the array to be used to process the audio
 		audio_array = np.frombuffer(data, dtype=np.float32)
 
 		detect_pitch(audio_array, SAMPLE_RATE)
-		
+
+		pygame.display.flip()
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT():
+				running = False
+
 if __name__ == '__main__':
 	main()
