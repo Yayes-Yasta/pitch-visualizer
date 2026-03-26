780e8ca5aa498bacda4ee9e97d9c3672ac4828e6
Yayes-Yasta <yayesyasta@gmail.com>
2026-03-26 21:43:25 +0400

    Refactored note on and off logic

--- src/game_logic.py
+++ src/game_logic.py
@@ -27,8 +27,8 @@ class Game:
 
 		self.instructions = instructions
 
-		self.current_notes = []
-		self.midway_notes = {}
+		self.current_notes = [] 
+		self.midway_notes = {} # notes that extend beyond the screen
 
 		self.midi_instructions = instructions
 		self.next_instruction = next(instructions)
@@ -37,6 +37,7 @@ class Game:
 
 	def update(self, mic_pitch, time):
 		"""Handles the drawing of the entire display"""
+
 		if self.done: return
 
 		self.time = time
@@ -55,12 +56,16 @@ class Game:
 		pygame.draw.circle(self.screen, color, position, radius)
 
 	def draw_expected_pitch_bars(self):
+		"""Draws the note bars of the current and misway notes"""
+
 		ahead_time = self.time + BASE_LINE_Y / PIXELS_PER_SECOND
 
 		self.update_active_notes(ahead_time)
 
 		notes = self.current_notes
 		print(notes)
+
+		# iterate through the notes completely visible on screen 
 		for note in notes:
 			start_time = note["start_time"]
 			end_time = note["end_time"]
@@ -73,6 +78,7 @@ class Game:
 			rectangle = pygame.Rect(x, y, width, height)
 			pygame.draw.rect(self.screen, BAR_COLOR, rectangle)
 
+		# iterate through the unfinished notes where the end is off screen 
 		for i in self.midway_notes:
 			note = self.midway_notes[i]
 
@@ -91,6 +97,9 @@ class Game:
 		pass
 
 	def update_active_notes(self, ahead_time):
+		"""Fetches the next midi instructions if necessary 
+		and adds notes to active_notes and midway_notes"""
+
 		print(ahead_time, self.next_instruction["time_since_start"])
 		while ahead_time > self.next_instruction["time_since_start"]:
 			instruction = self.next_instruction
@@ -99,21 +108,10 @@ class Game:
 			instruction_time = instruction["time_since_start"]
 
 			if instruction["type"] == "note_on":
-				if self.midway_notes.get(channel): 
-					self.midway_notes[channel]["end_time"] = instruction_time
-					self.current_notes.append(self.midway_notes[channel])
-				self.midway_notes[channel] = {
-					"note": instruction["note"],
-					"start_time": instruction_time,
-					"end_time": None
-				}
+				self.note_on(channel, instruction_time, instruction["note"])
 
 			else:
-				print("note_off detected")
-				note = self.midway_notes[channel]
-
-				note["end_time"] = instruction_time
-				self.current_notes.append(note)
+				self.note_off(channel, instruction_time)
 
 			print("getting the next instruction")
 
@@ -124,5 +122,33 @@ class Game:
 				self.done = True
 				print("End reached")
 				break
+
+	def note_on(self, channel, instruction_time, note):
+		"""Handles the start of a note"""
+
+		# if the same channel is already playing a note, stop that note
+		if self.midway_notes.get(channel): 
+			self.note_off(channel, instruction_time)
+
+		# adds the note to the unfinished notes
+		self.midway_notes[channel] = {
+			"note": note,
+			"start_time": instruction_time,
+			"end_time": None
+		}
+
+	def note_off(self, channel, instruction_time):
+		"""Handles the stop of a note"""
+
+		print("note_off detected")
+		note = self.midway_notes[channel]
+
+		# adds note to the finished notes
+		note["end_time"] = instruction_time
+		self.current_notes.append(note)
+
+		# removes note from the unfinished notes
+		del self.midway_notes[channel]
+
 
 
