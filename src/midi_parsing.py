06b5588cd8b9c84016b5e1a621d1d27d8ce3681f
Yayes-Yasta <yayesyasta@gmail.com>
2026-04-15 20:34:16 +0400

    Notes with velocity = 0 now count as note_off (The last time this was implemented, this change wasn't included in the commit for some reason)

--- src/midi_parsing.py
+++ src/midi_parsing.py
@@ -2,66 +2,72 @@ import mido
 
 
 class MidiParser:
-	"""This class is an iterator that reads a midi file 
-	and calculates the time at which instructions occur"""
+    """This class is an iterator that reads a midi file 
+    and calculates the time at which instructions occur"""
 
-	def __init__(self, file_path):
-		"""Prepares an array of all raw instructions parsed through mido."""
+    def __init__(self, file_path):
+        """Prepares an array of all raw instructions parsed through mido."""
 
-		messages = []
-		file = mido.MidiFile(file_path)
-		for msg in file:
-			messages.append(msg)
+        messages = []
+        file = mido.MidiFile(file_path)
+        for msg in file:
+            messages.append(msg)
 
-		self.instructions = messages # queue representing all midi instructions
-		self.time = 0 # time elapsed since start of the music
+        self.instructions = messages # queue representing all midi instructions
+        self.time = 0 # time elapsed since start of the music
 
-	def __iter__(self):
-		"""Returns an iterator object."""
+    def __iter__(self):
+        """Returns an iterator object."""
 
-		return self
+        return self
 
-	def __next__(self):
-		"""Returns needed information about the next instruction"""
+    def __next__(self):
+        """Returns needed information about the next instruction"""
 
-		instruction = self.instructions.pop(0)
+        instruction = self.instructions.pop(0)
 
-		if instruction.type == "end_of_track":
-			raise StopIteration
+        if instruction.type == "end_of_track":
+            raise StopIteration
 
-		# in case the instruction is not related to the played notes
-		while instruction.type != "note_on" and instruction.type != "note_off":
-			if instruction.type == "end_of_track":
-				raise StopIteration
+        # in case the instruction is not related to the played notes
+        while instruction.type != "note_on" and instruction.type != "note_off":
+            if instruction.type == "end_of_track":
+                raise StopIteration
 
-			instruction = self.instructions.pop(0)
+            instruction = self.instructions.pop(0)
 
-		self.add_time(instruction.time)
+        self.add_time(instruction.time)
+        
+        instruction_type = instruction.type
 
-		return {
-			"type": instruction.type, 
-			"channel": instruction.channel, 
-			"note": instruction.note, 
-			"time_since_start": self.time
-		}
+        # instructions with a velocity of 0 signify the end of a note
+        if instruction.velocity == 0:
+            instruction_type = "note_off"
 
-	def add_time(self, time):
-		"""adds time to the total time elapsed"""
+        return {
+            "type": instruction_type, 
+            "channel": instruction.channel, 
+            "note": instruction.note, 
+            "time_since_start": self.time
+        }
 
-		self.time += time
+    def add_time(self, time):
+        """adds time to the total time elapsed"""
 
-	def look_for_end(self, note: int, current_time) -> int:
-		"""Returns the next note_off instruction of a note that will happen"""
-		
-		instructions = self.instructions
+        self.time += time
 
-		end_time = current_time
+    def look_for_end(self, note: int, current_time) -> int:
+        """Returns the next note_off instruction of a note that will happen"""
+        
+        instructions = self.instructions
 
-		for instruction in instructions:
-			end_time += instruction.time
+        end_time = current_time
 
-			if instruction.type == "note_off" and instruction.note == note:
-				return end_time 
+        for instruction in instructions:
+            end_time += instruction.time
 
-		print("Error: Failed to find the end of the note")
-		return None
+            if instruction.type == "note_off" and instruction.note == note:
+                return end_time 
+
+        print("Error: Failed to find the end of the note")
+        return None
