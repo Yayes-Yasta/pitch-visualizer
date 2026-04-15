a18cc1aee8cae85c86fe07d0aac5eb70f66124b6
Yayes-Yasta <yayesyasta@gmail.com>
2026-04-16 00:43:47 +0400

    aubio_o object is now created outside of detect_pitch function because it only needs to be created once

--- src/pitch_detection.py
+++ src/pitch_detection.py
@@ -5,13 +5,13 @@ import numpy as np
 PITCH_TOLERANCE = 0.7 # minimum confidence for pitch candidates
 HOP_SIZE = 512 # number of samples for each pitch calculation
 
+# create aubio.pitch object
+pitch_o = aubio.pitch("yin", hop_size=HOP_SIZE, samplerate=sample_rate)
+pitch_o.set_tolerance(PITCH_TOLERANCE)
+pitch_o.set_unit("midi")
+
 def detect_pitch(audio_array, sample_rate):
 	"""Estimates and returns the pitch of a raw audio_array"""
-
-	# create aubio.pitch object
-	pitch_o = aubio.pitch("yin", hop_size=HOP_SIZE, samplerate=sample_rate)
-	pitch_o.set_tolerance(PITCH_TOLERANCE)
-	pitch_o.set_unit("midi")
 
 	# pad end of array with zeroes to make the length a multiple of hop_size
 	array_length = audio_array.shape[0]
